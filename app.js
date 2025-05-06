915c7ccb4971657bd3bcca9f5474c8709c03fde0
AKHILESH CHAKALI <23x51a0503@srecnandyal.edu.in>
2025-05-06 17:43:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,489 @@
+var newMemberAddBtn = document.querySelector('.addMemberBtn'),
+darkBg = document.querySelector('.dark_bg'),
+popupForm = document.querySelector('.popup'),
+crossBtn = document.querySelector('.closeBtn'),
+submitBtn = document.querySelector('.submitBtn'),
+ modalTitle = document.querySelector('.modalTitle'),
+ popupFooter = document.querySelector('.popupFooter'),
+form = document.querySelector('form'),
+ formInputFields = document.querySelectorAll('form input'),
+  fName = document.getElementById("fName"),
+  lName = document.getElementById("lName"),
+  age = document.getElementById("age"),
+  city = document.getElementById("city"),
+  position = document.getElementById("position"),
+  salary = document.getElementById("salary"),
+  sDate = document.getElementById("sDate"),
+ stat = document.getElementById("stat"),
+  email = document.getElementById("email"),
+  phone = document.getElementById("phone"),
+ guide = document.getElementById("guide"),
+  entries = document.querySelector(".showEntries"),
+  tabSize = document.getElementById("table_size"),
+  userInfo = document.querySelector(".userInfo"),
+  table = document.querySelector("table"),
+  filterData = document.getElementById("search")
+
+let originalData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+let getData = [...originalData]
+
+
+let isEdit = false, editId
+
+var arrayLength = 0
+var tableSize = 10
+var startIndex = 1
+var endIndex = 0
+var currentIndex = 1
+var maxIndex = 0
+
+showInfo()
+
+
+
+
+newMemberAddBtn.addEventListener('click', ()=> {
+    isEdit = false
+    submitBtn.innerHTML = "Submit"
+    modalTitle.innerHTML = "Fill the Form"
+    popupFooter.style.display = "block"
+    
+    darkBg.classList.add('active')
+    popupForm.classList.add('active')
+})
+
+crossBtn.addEventListener('click', ()=>{
+    darkBg.classList.remove('active')
+    popupForm.classList.remove('active')
+    form.reset()
+})
+
+document.getElementById("existingUserBtn").addEventListener("click", function() {
+    document.getElementById("userSelection").style.display = "none";
+    document.getElementById("image").style.display = "none";
+    document.getElementById("crudApp").style.display = "block";
+    document.querySelector(".container").style.display = "block"; // Fixed selector
+    document.getElementById("titleContainer").style.display = "none";
+    // Retrieve and display student details
+    document.querySelector(".userInfo").innerHTML = showInfo(); 
+
+    // Ensure navigation buttons are displayed
+    displayIndexBtn();
+});
+
+        document.getElementById("goBackBtn").addEventListener("click", function() {
+            document.getElementById("userSelection").style.display = "block";
+            document.getElementById("crudApp").style.display = "none";
+        
+        });
+
+
+function preLoadCalculations(){
+    array = getData
+    arrayLength = array.length
+    maxIndex = arrayLength / tableSize
+
+    if((arrayLength % tableSize) > 0){
+        maxIndex++
+    }
+}
+
+
+
+function displayIndexBtn(){
+    preLoadCalculations()
+
+    const pagination = document.querySelector('.pagination')
+
+    pagination.innerHTML = ""
+
+    pagination.innerHTML = '<button onclick="prev()" class="prev">Previous</button>'
+
+    for(let i=1; i<=maxIndex; i++){
+        pagination.innerHTML += '<button onclick= "paginationBtn('+i+')" index="'+i+'">'+i+'</button>'
+    }
+
+    pagination.innerHTML += '<button onclick="next()" class="next">Next</button>'
+
+    highlightIndexBtn()
+}
+
+
+function highlightIndexBtn(){
+    startIndex = ((currentIndex - 1) * tableSize) + 1
+    endIndex = (startIndex + tableSize) - 1
+
+    if(endIndex > arrayLength){
+        endIndex = arrayLength
+    }
+
+    if(maxIndex >= 2){
+        var nextBtn = document.querySelector(".next")
+        nextBtn.classList.add("act")
+    }
+
+
+
+    entries.textContent = `Showing ${startIndex} to ${endIndex} of ${arrayLength} entries`
+
+    var paginationBtns = document.querySelectorAll('.pagination button')
+    paginationBtns.forEach(btn => {
+        btn.classList.remove('active')
+        if(btn.getAttribute('index') === currentIndex.toString()){
+            btn.classList.add('active')
+        }
+    })
+
+
+    showInfo()
+}
+
+
+
+function validateForm() {
+    
+    let Phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    
+   
+    let mobilePattern = /^[0-9]{10}$/;
+
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!mobilePattern.test(Phone)) {
+        return false;
+    }
+
+    if (!emailPattern.test(email)) {
+    return false;
+    }
+   
+    }
+
+
+function showInfo(){
+    document.querySelectorAll(".employeeDetails").forEach(info => info.remove())
+
+    var tab_start = startIndex - 1
+    var tab_end = endIndex
+
+    if(getData.length > 0){
+        for(var i=tab_start; i<tab_end; i++){
+            var staff = getData[i]
+
+
+              if(staff){
+                let createElement = `<tr class = "employeeDetails">
+                <td>${i+1}</td>
+          
+                <td>${staff.fName + " " + staff.lName}</td>
+                <td>${staff.ageVal}</td>
+                <td>${staff.cityVal}</td>
+                <td>${staff.positionVal}</td>
+                <td>${staff.salaryVal}</td>
+                <td>${staff.sDateVal}</td>
+                <td>${staff.statVal}</td>
+                <td>${staff.emailVal}</td>
+                <td>${staff.phoneVal}</td>
+                <td>${staff.guideVal}</td>
+                <td>
+                    <button onclick="readInfo('${staff.fName}', '${staff.lName}', '${staff.ageVal}', '${staff.cityVal}', '${staff.positionVal}', '${staff.salaryVal}', '${staff.sDateVal}','${staff.statVal}','${staff.emailVal}', '${staff.phoneVal}','${staff.guideVal}')"><i class="fa-regular fa-eye"></i></button>
+
+                    <button onclick="editInfo('${i}','${staff.fName}', '${staff.lName}', '${staff.ageVal}', '${staff.cityVal}', '${staff.positionVal}', '${staff.salaryVal}', '${staff.sDateVal}','${staff.statVal}','${staff.emailVal}', '${staff.phoneVal}','${staff.guideVal}')"><i class="fa-regular fa-pen-to-square"></i></button>
+
+
+                    <button onclick = "deleteInfo(${i})"><i class="fa-regular fa-trash-can"></i></button>
+                </td>
+            </tr>`
+
+                userInfo.innerHTML += createElement
+                table.style.minWidth = "1400px"
+            }
+        }
+    }
+
+    else{
+        userInfo.innerHTML = `<tr class="employeeDetails"><td class="empty" colspan="11" align="center">No data available in table</td></tr>`
+        table.style.minWidth = "1400px"
+    }
+
+}
+
+showInfo()
+
+
+function readInfo(fname, lname, Age, City, Position, Salary, SDate,Stat, Email, Phone,Guide){
+
+    fName.value = fname
+    lName.value = lname
+    age.value = Age
+    city.value = City
+    position.value = Position
+    salary.value = Salary
+    sDate.value = SDate
+     stat.value = Stat
+    email.value = Email
+    phone.value = Phone
+    guide.value = Guide
+
+    darkBg.classList.add('active')
+    popupForm.classList.add('active')
+    popupFooter.style.display = "none"
+    modalTitle.innerHTML = "Profile"
+    formInputFields.forEach(input => {
+        input.disabled = true
+    })
+
+
+    
+}
+
+function editInfo(id,  fname, lname, Age, City, Position, Salary, SDate,Stat, Email, Phone,Guide){
+    isEdit = true
+    editId = id
+
+    // Find the index of the item to edit in the original data based on id
+    const originalIndex = originalData.findIndex(item => item.id === id)
+
+    // Update the original data
+    originalData[originalIndex] = {
+        id: id,
+       
+        fName: fname,
+        lName: lname,
+        ageVal: Age,
+        cityVal: City,
+        positionVal: Position,
+        salaryVal: Salary,
+        sDateVal: SDate,
+        statVal:Stat,
+        emailVal: Email,
+        phoneVal: Phone,
+        guideVal: Guide
+    }
+
+   
+    fName.value = fname
+    lName.value = lname
+    age.value = Age
+    city.value = City
+    position.value = Position
+    salary.value = Salary
+    sDate.value = SDate
+    stat.value = Stat
+    email.value = Email
+    phone.value = Phone
+    guide.value = Guide
+
+
+    darkBg.classList.add('active')
+    popupForm.classList.add('active')
+    popupFooter.style.display = "block"
+    modalTitle.innerHTML = "Update the Form"
+    submitBtn.innerHTML = "Update"
+    formInputFields.forEach(input => {
+        input.disabled = false
+    })
+
+
+ 
+}
+
+function deleteInfo(index){
+    if(confirm("Aer you sure want to delete?")){
+        originalData.splice(index, 1);
+        localStorage.setItem("userProfile", JSON.stringify(originalData));
+        
+        // Update getData after deleting the record
+        getData = [...originalData];
+
+        preLoadCalculations()
+
+        if(getData.length === 0){
+            currentIndex = 1
+            startIndex = 1
+            endIndex = 0
+        }
+        else if(currentIndex > maxIndex){
+            currentIndex = maxIndex
+        }
+
+        showInfo()
+        highlightIndexBtn()
+        displayIndexBtn()
+
+        var nextBtn = document.querySelector('.next')
+        var prevBtn = document.querySelector('.prev')
+
+        if(Math.floor(maxIndex) > currentIndex){
+            nextBtn.classList.add("act")
+        }
+        else{
+            nextBtn.classList.remove("act")
+        }
+
+
+        if(currentIndex > 1){
+            prevBtn.classList.add('act')
+        }
+    }
+}
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+
+    const information = {
+        id: Date.now(),
+       
+        fName: fName.value,
+        lName: lName.value,
+        ageVal: age.value,
+        cityVal: city.value,
+        positionVal: position.value,
+        salaryVal: salary.value,
+        sDateVal: sDate.value,
+        statVal: stat.value,
+        emailVal: email.value,
+        phoneVal: phone.value,
+        guideVal: guide.value
+    }
+
+    if(!isEdit){
+        originalData.unshift(information)
+    }
+    else{
+        originalData[editId] = information
+    }
+    getData = [...originalData]
+    localStorage.setItem('userProfile', JSON.stringify(originalData))
+
+    submitBtn.innerHTML = "Submit"
+    modalTitle.innerHTML = "Fill the Form"
+
+    darkBg.classList.remove('active')
+    popupForm.classList.remove('active')
+    form.reset()
+
+
+    highlightIndexBtn()
+    displayIndexBtn()
+    showInfo()
+
+    var nextBtn = document.querySelector(".next")
+    var prevBtn = document.querySelector(".prev")
+    if(Math.floor(maxIndex) > currentIndex){
+        nextBtn.classList.add("act")
+    }
+    else{
+        nextBtn.classList.remove("act")
+    }
+
+
+    if(currentIndex > 1){
+        prevBtn.classList.add("act")
+    }
+})
+
+
+function next(){
+    var prevBtn = document.querySelector('.prev')
+    var nextBtn = document.querySelector('.next')
+
+    if(currentIndex <= maxIndex - 1){
+        currentIndex++
+        prevBtn.classList.add("act")
+
+        highlightIndexBtn()
+    }
+
+    if(currentIndex > maxIndex - 1){
+        nextBtn.classList.remove("act")
+    }
+}
+
+
+function prev(){
+    var prevBtn = document.querySelector('.prev')
+
+    if(currentIndex > 1){
+        currentIndex--
+        prevBtn.classList.add("act")
+        highlightIndexBtn()
+    }
+
+    if(currentIndex < 2){
+        prevBtn.classList.remove("act")
+    }
+}
+
+
+function paginationBtn(i){
+    currentIndex = i
+
+    var prevBtn = document.querySelector('.prev')
+    var nextBtn = document.querySelector('.next')
+
+    highlightIndexBtn()
+
+    if(currentIndex > maxIndex - 1){
+        nextBtn.classList.remove('act')
+    }
+    else{
+        nextBtn.classList.add("act")
+    }
+
+
+    if(currentIndex > 1){
+        prevBtn.classList.add("act")
+    }
+
+    if(currentIndex < 2){
+        prevBtn.classList.remove("act")
+    }
+}
+
+
+
+tabSize.addEventListener('change', ()=>{
+    var selectedValue = parseInt(tabSize.value)
+    tableSize = selectedValue
+    currentIndex = 1
+    startIndex = 1
+    displayIndexBtn()
+})
+
+
+
+filterData.addEventListener("input", ()=> {
+    const searchTerm = filterData.value.toLowerCase().trim()
+
+    if(searchTerm !== ""){
+
+        const filteredData = originalData.filter((item) => {
+            const fullName = (item.fName + " " + item.lName).toLowerCase()
+            const city = item.cityVal.toLowerCase()
+            const position = item.positionVal.toLowerCase()
+
+            return(
+                fullName.includes(searchTerm) ||
+                city.includes(searchTerm) ||
+                position.includes(searchTerm)
+            )
+        })
+
+        // Update the current data with filtered data
+        getData = filteredData
+    }
+
+    else{
+        getData = JSON.parse(localStorage.getItem('userProfile')) || []
+    }
+
+
+    currentIndex = 1
+    startIndex = 1
+    displayIndexBtn()
+
+
+})
